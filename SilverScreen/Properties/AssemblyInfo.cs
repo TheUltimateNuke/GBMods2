572f6d0e301799bbe48d7cf5902ae7520fa4dd9b
The_UltimateNuke <84410206+TheUltimateNuke@users.noreply.github.com>
2024-11-29 13:24:41 -0500

    Add project files.

---
+++ SilverScreen/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+using System.Reflection;
+using BuildInfo = SilverScreen.Mod.BuildInfo;
+
+[assembly: AssemblyTitle(BuildInfo.Description)]
+[assembly: AssemblyDescription(BuildInfo.Description)]
+[assembly: AssemblyCompany(BuildInfo.Company)]
+[assembly: AssemblyProduct(BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + BuildInfo.Author)]
+[assembly: AssemblyTrademark(BuildInfo.Company)]
+[assembly: AssemblyVersion(BuildInfo.Version)]
+[assembly: AssemblyFileVersion(BuildInfo.Version)]
+
+[assembly: MelonInfo(typeof(SilverScreen.Mod), BuildInfo.Name, BuildInfo.Version, BuildInfo.Author)]
+[assembly: MelonGame("Boneloaf", "Gang Beasts")]
